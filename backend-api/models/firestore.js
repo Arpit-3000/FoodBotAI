498026a744533a43a6ab3911d160894d1933b31d
Arpit-3000 <arpitcollege1205@gmail.com>
2025-07-28 01:35:19 +0530

    Update firestore.js

--- backend-api/models/firestore.js
+++ backend-api/models/firestore.js
@@ -1,8 +1,10 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../serviceAccountKey.json');
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.FIREBASE_DATABASE_URL
 });
 
 const db = admin.firestore();
